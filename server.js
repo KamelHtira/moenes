31f68d1d4c61c9dac238e7bd8d60c8ab8b43a18b
KamelHtira <Kamel.htira@isamm.u-manouba.tn>
2022-12-08 00:56:56 +0100

    yaweldi commiti

--- server.js
+++ server.js
@@ -5,40 +5,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const usersSchema = mongoose.Schema(
-    {
-        username: {
-            type:String,
-            required:true,
-            unique: true 
-        },
-        password: {
-            type:String,
-            required:true
-        }
-    }
-)
-Users = mongoose.model('users',usersSchema)
+const usersSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+Users = mongoose.model("users", usersSchema);
 
-app.get("/api",async (req, res) => {
+app.post("/api", async (req, res) => {
   res.send("MOENES!!..");
-  try{
-
-    let new_user = new Users (
-      {
-        username: req.body.username,
-        password: req.body.password
-      }
-    )
-    console.log(new_user)
-    await  new_user.save();
-    res.send(`user is added successfully : ${new_user}`)
-    }
-    catch(err)
-    {
-      console.log(err) 
-      res.send(err)
-    }
+  try {
+    let new_user = new Users({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    console.log(new_user);
+    await new_user.save();
+    res.send(`user is added successfully : ${new_user}`);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
 
 // connecting server to mongoDB Atlas :
